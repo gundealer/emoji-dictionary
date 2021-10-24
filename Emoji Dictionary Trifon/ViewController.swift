28196e569edbab273ce7df869d1a939d7905a4ef
trifon.getsov <tgecov@gmail.com>
2021-10-25 00:56:03 +0300

    Initial Commit

---
+++ Emoji Dictionary Trifon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary Trifon
+//
+//  Created by Trifon Getsov on 25.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
